bc756db20beaaa4fafc427a43cd2b4e20c43b626
Julianny Restrepo <yuli6303@gmail.com>
2021-04-16 00:52:40 -0500

    created atom components

---
+++ spotyLike/src/app/components/atoms/common-input/common-input.component.ts
@@ -0,0 +1,22 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-common-input',
+  templateUrl: './common-input.component.html',
+  styleUrls: ['./common-input.component.scss'],
+})
+export class CommonInputComponent {
+  value: any = '';
+
+  @Input() placeholder: string = '';
+  @Input() type: string = '';
+  @Input() style: any = '';
+
+  @Output() actualValue: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() {}
+
+  updateValue() {
+    this.actualValue.emit(this.value);
+  }
+}
