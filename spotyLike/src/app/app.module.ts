e529c22c34e78427b6ab2aa96ddb530bde36b880
Julianny Restrepo <yuli6303@gmail.com>
2021-04-16 00:55:01 -0500

    modified style file, colors and added icons

--- spotyLike/src/app/app.module.ts
+++ spotyLike/src/app/app.module.ts
@@ -7,6 +7,19 @@ import { LogInComponent } from './pages/log-in/log-in.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { CommonButtonComponent } from './components/atoms/common-button/common-button.component';
+import { IconButtonComponent } from './components/atoms/icon-button/icon-button.component';
+import { CommonInputComponent } from './components/atoms/common-input/common-input.component';
+import { FormsModule } from '@angular/forms';
+import { LogoComponent } from './components/molecules/logo/logo.component';
+import { LogInFormComponent } from './components/molecules/log-in-form/log-in-form.component';
+import { OtherFormAccessComponent } from './components/molecules/other-form-access/other-form-access.component';
+import { NavigationBarComponent } from './components/organisms/navigation-bar/navigation-bar.component';
+import { MenuComponent } from './components/organisms/menu/menu.component';
+import { SongsListComponent } from './components/organisms/songs-list/songs-list.component';
+import { IconInputComponent } from './components/atoms/icon-input/icon-input.component';
+import { SignInButtonComponent } from './components/molecules/sign-in-button/sign-in-button.component';
+import { LogInOrganismComponent } from './components/organisms/log-in-org/log-in-org.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +28,20 @@ import { FavoritesComponent } from './pages/favorites/favorites.component';
     SignInComponent,
     HomeComponent,
     FavoritesComponent,
+    CommonButtonComponent,
+    IconButtonComponent,
+    CommonInputComponent,
+    LogoComponent,
+    LogInFormComponent,
+    LogInOrganismComponent,
+    OtherFormAccessComponent,
+    NavigationBarComponent,
+    MenuComponent,
+    SongsListComponent,
+    IconInputComponent,
+    SignInButtonComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- spotyLike/src/app/pages/log-in/log-in.component.ts
+++ spotyLike/src/app/pages/log-in/log-in.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrls: ['./log-in.component.scss']
 })
 export class LogInComponent implements OnInit {
+  value: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.value = '';
   }
 
+  ngOnInit(): void {}
+
+  showLabel() {
+    console.log(this.value);
+  }
+
+  getValue(event: any) {
+    this.value = event;
+  }
 }
